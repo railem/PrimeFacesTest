25cc77300033ec612f42a7aa6a0ad74326581a19
railem <glathor@hotmail.de>
2018-08-06 15:45:06 +0200

    init

---
+++ src/main/java/org/jboss/as/quickstarts/greeter/domain/User.java
@@ -0,0 +1,84 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.greeter.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.*;
+
+@Entity
+// User is a keyword in some SQL dialects!
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    @Size( min=4, max=24 )
+    private String username;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @Min(0)
+    @Max(130)
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/org/jboss/as/quickstarts/greeter/web/ListController.java
@@ -0,0 +1,60 @@
+package org.jboss.as.quickstarts.greeter.web;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.jboss.as.quickstarts.greeter.domain.User;
+import org.jboss.as.quickstarts.greeter.domain.UserDao;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
+
+import java.util.List;
+
+@Named
+@RequestScoped
+public class ListController {
+
+    @Inject
+    private UserDao userDao;
+
+    private List<User> users;
+
+    private User selectedUser;
+
+    public List<User> getUsers() {
+        users = userDao.getAllUsers();
+
+        return users;
+    }
+
+    public User getSelectedUser() {
+        return selectedUser;
+    }
+
+    public void setSelectedUser( User user ) {
+        this.selectedUser = user;
+    }
+
+    public void onDoubleClick(SelectEvent event) {
+
+        //selectedUser = (User) event.getObject();
+        RequestContext context = RequestContext.getCurrentInstance();
+        context.execute("PF('dlg2').show();");
+
+        System.out.println("CLICK");
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        FacesMessage msg = new FacesMessage("Car Selected", ((User) event.getObject()).getId() + "");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onRowUnselect(UnselectEvent event) {
+        FacesMessage msg = new FacesMessage("Car Unselected", ((User) event.getObject()).getId() + "");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+}
